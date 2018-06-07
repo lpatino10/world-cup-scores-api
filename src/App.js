6fb43a4b8178c96eb739e393de2952ebb6839e4f
Logan Patino <loganpatino10@gmail.com>
2018-06-07 01:23:55 -0400

    feat(api): Get today's scheduled matches

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   componentDidMount() {
+    const now = moment();
+
+    // for testing, since we haven't started yet
+    //const now = moment().add(7, 'days');
+
+    const currentDateString = now.format('YYYY-MM-DD');
+
+    const fetchUrl = `//api.football-data.org/v1/competitions/467/fixtures?timeFrameStart=${currentDateString}&timeFrameEnd=${currentDateString}`;
     const fetchOptions = {
       headers: {
         'X-Auth-Token': process.env.REACT_APP_API_KEY,
       },
     };
 
-    fetch('//api.football-data.org/v1/competitions/467', fetchOptions)
-      .then(res => res.json())
-      .then(body => console.log(body.caption));
+    fetch(fetchUrl, fetchOptions)
+      .then(response => response.json())
+      .then(body => console.log(body));
 
     //setInterval(() => { }, 10000);
   }
